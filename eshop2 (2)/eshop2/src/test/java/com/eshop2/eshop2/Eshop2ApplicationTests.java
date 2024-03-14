6428ec43743f73a80a49f1d8a8ca811baff77a8f
Kunal Kamble <97936261+KunalKamble1234@users.noreply.github.com>
2024-03-14 23:37:33 +0530

    Add files via upload

---
+++ eshop2 (2)/eshop2/src/test/java/com/eshop2/eshop2/Eshop2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eshop2.eshop2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eshop2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
